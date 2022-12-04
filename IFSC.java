0e650c90d36cc5ffd9a0cbca18b72b9e3eb15496
Bhanu Pratap Singh <115410285+VelociRexy@users.noreply.github.com>
2022-12-04 14:52:01 +0530

    IFSC.java

--- IFSC.java
+++ IFSC.java
@@ -2,13 +2,12 @@ import java.util.*;
 import java.io.*;
 class Main{
   public static void main(String args[]) throws Exception{
-    FileReader fr=new FileReader("ifsc.json");
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     System.out.print("Enter Bank Code : ");
     String code=br.readLine();
-    Scanner sc=new Scanner(fr);
-    boolean found=false;
-    String s="Code ";
+    String s=code.substring(0,4);
+    FileReader fr=new FileReader(s+".json");
+    Scanner sc=new Scanner(fr);    
     while(sc.hasNextLine())
     {
       StringTokenizer st=new StringTokenizer(s);
@@ -16,15 +15,15 @@ class Main{
         s=sc.next().replace('\"',' ');   
       else
       {
-        found=true;
         sc.nextLine();
         String s2=sc.nextLine().trim();
         s2=s2.replace('\"',' ');
         while(!s2.equals("},"))
         {
+          if(s2.contains("STATE")||s2.contains("BRANCH")||s2.contains("IFSC")||s2.contains("DISTRICT")||s2.contains("ADDRESS"))
           System.out.println(s2.replace(',',' '));
-          s2=sc.nextLine().trim();
-          s2=s2.replace('\"',' ');
+          s2=sc.nextLine();
+          s2=s2.replace('\"',' ').trim();
         }
         sc.close();
         break;
